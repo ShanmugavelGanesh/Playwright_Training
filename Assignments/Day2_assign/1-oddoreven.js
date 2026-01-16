7069ed4ffa4cc917646bb152ca04a2e9e96ef1a8
ShanmugavelGanesh <helloshanmugavel@gmail.com>
2026-01-16 10:40:57 +0530

    Add assignments only

---
+++ Assignments/Day2_assign/1-oddoreven.js
@@ -0,0 +1,16 @@
+//Print the given number is Odd or Even
+
+function isOddOrEven(){
+    let num;
+    for (num=1;num<=20;num++){
+    if(num%2==0){
+        console.log(num +' is Even');
+    } else{
+        console.log(num +' is Odd');
+        
+    }
+ 
+}
+}
+
+isOddOrEven()
